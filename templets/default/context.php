552d862448b973e3ba71e206f458270bcf036a09
arzzcom <arzz@arzz.com>
2018-07-14 22:55:07 +0900

    버그수정

--- templets/default/context.php
+++ templets/default/context.php
@@ -8,7 +8,7 @@
  * @author Arzz (arzz@arzz.com)
  * @license MIT License
  * @version 3.0.0
- * @modified 2018. 5. 21.
+ * @modified 2018. 7. 14.
  */
 if (defined('__IM__') == false) exit;
 ?>
@@ -16,7 +16,7 @@ if (defined('__IM__') == false) exit;
 	<div>
 		<ul>
 			<?php for ($i=0, $loop=count($contexts);$i<$loop;$i++) { ?>
-			<li<?php echo $contexts[$i]->tab == $tab ? ' class="selected"' : ''; ?>><a href="<?php echo $me->getUrl($contexts[$i]->tab,false); ?>"><?php echo $contexts[$i]->title; ?></a></li>
+			<li<?php echo $contexts[$i]->tab == $tab->tab ? ' class="selected"' : ''; ?>><a href="<?php echo $me->getUrl($contexts[$i]->tab,false); ?>"><?php echo $contexts[$i]->title; ?></a></li>
 			<?php } ?>
 		</ul>
 	</div>
